c7e1923ce9db92fa386b96f98e52e9245f2039e1
Midas Trisha <mtrisha@student.21-school.ru>
2020-01-19 12:48:56 +0300

    add link to wiki

--- sources/main.c
+++ sources/main.c
@@ -6,12 +6,16 @@
 /*   By: Kashnitskiy <elijahkash.code@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 15:14:46 by Kashnitskiy       #+#    #+#             */
-/*   Updated: 2020/01/18 16:47:51 by Kashnitskiy      ###   ########.fr       */
+/*   Updated: 2020/01/19 12:48:21 by Kashnitskiy      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
+/*
+** Look here if you don’t understand the meaning of such main()
+** https://github.com/elijahkash/libft/wiki/First-steps
+*/
 int		main(void)
 {
 	ft_memman_init();
